3910f48b7d0884d27c1d55cd5795d1f06b0d47e9
Guido Molina <guidoo840@gmail.com.ar>
2024-06-15 23:48:42 -0300

    actualizacion de repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './pages/header-component/header-component.component';
+import { BodyComponentComponent } from './pages/body-component/body-component.component';
+import { FooterComponentComponent } from './pages/footer-component/footer-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    BodyComponentComponent,
+    FooterComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/body-component/body-component.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-component',
+  templateUrl: './body-component.component.html',
+  styleUrl: './body-component.component.css'
+})
+export class BodyComponentComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
