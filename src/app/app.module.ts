b393d8f9c315146b3eaca13e557a834c28bdc213
Guido Molina <guidoo840@gmail.com.ar>
2024-06-20 21:07:42 -0300

    config routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './pages/header-component/header-component.component';
 import { BodyComponentComponent } from './pages/body-component/body-component.component';
 import { FooterComponentComponent } from './pages/footer-component/footer-component.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
 
 @NgModule({
   declarations: [
@@ -18,7 +30,26 @@ import { FooterComponentComponent } from './pages/footer-component/footer-compon
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({"projectId":"app-anime-wiki","appId":"1:1030772770136:web:6bf00be6e211953bcf112d","databaseURL":"https://app-anime-wiki-default-rtdb.firebaseio.com","storageBucket":"app-anime-wiki.appspot.com","apiKey":"AIzaSyB4aYNlRoD6AW52DpPK-8MLo5Dn3T_53Yk","authDomain":"app-anime-wiki.firebaseapp.com","messagingSenderId":"1030772770136"})),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideAppCheck(() => {
+      // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+      const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+      return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+    }),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideVertexAI(() => getVertexAI())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
